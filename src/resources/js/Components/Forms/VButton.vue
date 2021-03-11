1f38e039fbbbd9e11cff6078a818919891ef08c6
Mar4elius <marauman@gmail.com>
2021-03-10 22:49:41 -0700

    Change name of button component

--- src/resources/js/Components/Forms/VButton.vue
+++ src/resources/js/Components/Forms/VButton.vue
@@ -1,6 +1,5 @@
 <template>
 	<button
-		:type="type"
 		class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
 	>
 		<slot></slot>
@@ -10,9 +9,9 @@
 <script>
 	export default {
 		props: {
-			type: {
-				type: String,
-				default: 'submit'
+			onClick: {
+				type: Function,
+				required: true
 			}
 		}
 	};
