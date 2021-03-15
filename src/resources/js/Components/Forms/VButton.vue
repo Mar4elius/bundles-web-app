764c145dc0fa4643ea7c1ef90db346bac4374fce
Mar4elius <marauman@gmail.com>
2021-03-14 20:51:02 -0600

    Create v-button vue component

--- src/resources/js/Components/Forms/VButton.vue
+++ src/resources/js/Components/Forms/VButton.vue
@@ -1,18 +1,42 @@
 <template>
-	<button
-		class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
-	>
+	<button :type="button" :class="combinedClasses" :disabled="isDisabled">
 		<slot></slot>
 	</button>
 </template>
 
 <script>
+	import { computed } from '@vue/runtime-core';
 	export default {
 		props: {
 			onClick: {
 				type: Function,
 				required: true
+			},
+
+			type: {
+				type: String,
+				default: 'button'
+			},
+
+			classes: {
+				type: String,
+				default: ' '
+			},
+
+			isDisabled: {
+				type: Boolean,
+				default: false
 			}
+		},
+
+		setup(props) {
+			let defaultClasses = `px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10 `;
+
+			const combinedClasses = computed(() => defaultClasses.concat(props.classes));
+
+			return {
+				combinedClasses
+			};
 		}
 	};
 </script>
