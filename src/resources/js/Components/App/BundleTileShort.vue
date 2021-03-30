52d7cddae355b97aa92eda4d2753c089bf6b2cdf
Mar4elius <marauman@gmail.com>
2021-03-30 15:31:04 -0600

    Fix display issue for table rows

--- src/resources/js/Components/App/BundleTileShort.vue
+++ src/resources/js/Components/App/BundleTileShort.vue
@@ -1,22 +1,27 @@
 <template>
 	<div class="mx-5 mb-5 sm:mx-6 sm:mb-6">
 		<div class="h-full flex content-between flex-wrap">
-			<img :src="bundle.image_path" :alt="bundle.name" />
-			<h4>{{ bundle.name }}</h4>
-			<table class="w-full mb-5">
-				<thead>
-					<tr>
-						<th class="text-left">Items</th>
-						<th class="text-right">Quantity</th>
-					</tr>
-				</thead>
-				<tbody>
-					<tr v-for="product in bundle.products" :key="product.slug">
-						<td>{{ product.name }}</td>
-						<td class="text-center">{{ product.pivot.default_quantity }}</td>
-					</tr>
-				</tbody>
-			</table>
+			<div>
+				<img :src="bundle.image_path" :alt="bundle.name" />
+				<h4>{{ bundle.name }}</h4>
+				<table class="w-full mb-5">
+					<thead>
+						<tr>
+							<th class="text-left">Items</th>
+							<th class="text-right">Quantity</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr v-for="product in bundle.products" :key="product.slug">
+							<td class="w-4/5">{{ product.name }}</td>
+							<td class="text-center w-1/5">{{ product.pivot.default_quantity }}</td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+			<div>
+				<h4>Great for:</h4>
+			</div>
 			<bundle-details-buttons :bundle="bundle" />
 		</div>
 	</div>
