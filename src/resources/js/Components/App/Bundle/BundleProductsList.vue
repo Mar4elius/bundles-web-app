deae75a623adc0ac1fc69b40a274e832a5ab6e85
Mar4elius <marauman@gmail.com>
2021-04-06 15:29:39 -0600

    Create bundle products list component

---
+++ src/resources/js/Components/App/Bundle/BundleProductsList.vue
@@ -0,0 +1,26 @@
+<template>
+	<table class="mb-5 w-full">
+		<thead>
+			<tr>
+				<th class="text-left">Items</th>
+				<th class="text-right">Quantity</th>
+			</tr>
+		</thead>
+		<tbody>
+			<tr v-for="product in products" :key="product.slug">
+				<td class="w-4/5">{{ product.name }}</td>
+				<td class="text-center w-1/5">{{ product.pivot.default_quantity }}</td>
+			</tr>
+		</tbody>
+	</table>
+</template>
+<script>
+	export default {
+		props: {
+			products: {
+				type: Array,
+				required: true
+			}
+		}
+	};
+</script>
