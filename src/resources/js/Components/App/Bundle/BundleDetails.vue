520f3c1ef3c343ad69b24911cee444b77d2cb66b
Mar4elius <marauman@gmail.com>
2021-04-06 17:05:31 -0600

    Minor style changes for bundle details page

--- src/resources/js/Components/App/Bundle/BundleDetails.vue
+++ src/resources/js/Components/App/Bundle/BundleDetails.vue
@@ -1,7 +1,7 @@
 <template>
 	<div class="bg-white rounded-md p-4 md:p-6">
 		<div class="container mx-auto">
-			<div class="md:flex">
+			<div class="md:flex items-center">
 				<div class="w-full h-64 md:w-1/2 lg:h-96">
 					<img
 						class="h-full w-full rounded-md object-cover max-w-xl"
@@ -27,8 +27,8 @@
 						<bundle-products-list :products="data.bundle.products" />
 					</div>
 
-					<div class="mt-2 flex items-center">
-						<label class="text-gray-700" for="count">Count:</label>
+					<div class="mt-2">
+						<h6 class="" for="count">Count:</h6>
 						<div class="flex items-center mt-1">
 							<button
 								class="text-gray-500 focus:outline-none hover:text-indigo-600"
@@ -69,10 +69,9 @@
 						</div>
 					</div>
 					<div class="flex items-center mt-6">
-						<v-button-filled id="order-now"> Order Now </v-button-filled>
-						<button class="mx-2 text-gray-600 border rounded-md p-2 hover:bg-gray-200 focus:outline-none">
+						<v-button-filled id="order-now" classes="w-32 flex justify-center">
 							<svg
-								class="h-5 w-5"
+								class="w-6 md:w-8"
 								fill="none"
 								stroke-linecap="round"
 								stroke-linejoin="round"
@@ -84,7 +83,7 @@
 									d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
 								></path>
 							</svg>
-						</button>
+						</v-button-filled>
 					</div>
 				</div>
 			</div>
