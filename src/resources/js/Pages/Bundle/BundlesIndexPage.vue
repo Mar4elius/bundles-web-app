73bc4988ff65cffdb066217931e12f0a318bd370
Mar4elius <marauman@gmail.com>
2021-04-05 21:37:09 -0600

    Add loading animation to home page

--- src/resources/js/Pages/Bundle/BundlesIndexPage.vue
+++ src/resources/js/Pages/Bundle/BundlesIndexPage.vue
@@ -8,7 +8,8 @@
 			<div class="w-full flex justify-start sm:justify-center">
 				<h1>Our most popular bundles:</h1>
 			</div>
-			<bundle-tile-long v-for="bundle in bundlesByPopularity" :key="bundle.id" :bundle="bundle" />
+			<whole-page-loading-animation v-if="isLoading" />
+			<bundle-tile-long v-else v-for="bundle in bundlesByPopularity" :key="bundle.id" :bundle="bundle" />
 		</template>
 	</app-layout>
 </template>
@@ -16,22 +17,25 @@
 <script>
 	// Vue
 	import { useStore } from 'vuex';
-	import { onMounted, ref } from 'vue';
+	import { computed, onMounted, ref } from 'vue';
 	// Components
 	import HeroBanner from '@/Pages/HeroBanner';
 	import AppLayout from '@/Layouts/AppLayout';
 	import BundleTileLong from '@/Components/App/Bundle/BundleTileLong';
+	import WholePageLoadingAnimation from '@/Components/Support/WholePageLoadingAnimation';
 
 	export default {
 		components: {
 			AppLayout,
 			BundleTileLong,
-			HeroBanner
+			HeroBanner,
+			WholePageLoadingAnimation
 		},
 
 		setup() {
 			const store = useStore();
 			let bundlesByPopularity = ref([]);
+			const isLoading = computed(() => store.state.loader.loading);
 
 			async function getTopTenBundles(sortBy) {
 				const { data } = await store.dispatch('bundles/getTopTenBundles', sortBy);
@@ -41,7 +45,8 @@
 			onMounted(getTopTenBundles('popularity'));
 
 			return {
-				bundlesByPopularity
+				bundlesByPopularity,
+				isLoading
 			};
 		}
 	};
