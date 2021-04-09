ddc6a5495508569c1ef59ca1982e2bcb9770d999
Mar4elius <marauman@gmail.com>
2021-04-09 13:22:19 -0600

    Create function that create random string

--- src/resources/js/helpers.js
+++ src/resources/js/helpers.js
@@ -21,4 +21,22 @@ function calculatePrice(price) {
 	return price.toLocaleString('en-CA', { style: 'currency', currency: 'CAD' });
 }
 
-export { capitalizeFirstLetter, calculatePrice };
+/**
+ * Generate random alpha numeric string
+ *
+ * @param {Number} length
+ *
+ * @return String
+ */
+function randomAlphaNumericString(length) {
+	let result = [];
+	let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+	let charactersLength = characters.length;
+
+	for (let i = 0; i < length; i++) {
+		result.push(characters.charAt(Math.floor(Math.random() * charactersLength)));
+	}
+	return result.join('');
+}
+
+export { capitalizeFirstLetter, calculatePrice, randomAlphaNumericString };
