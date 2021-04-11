1fef69b87819af5372d616f9a893b494df22c5e5
Mar4elius <marauman@gmail.com>
2021-04-11 13:56:31 -0600

    Remove add to cart bundle from simple tile component

--- src/resources/js/Components/App/Bundle/BundleTileSimple.vue
+++ src/resources/js/Components/App/Bundle/BundleTileSimple.vue
@@ -6,7 +6,7 @@
 		<div class="px-5 py-3">
 			<h6 class="text-gray-700 uppercase text-sm">{{ bundle.name }}</h6>
 			<span class="text-gray-500 mt-3">{{ calculatePrice(bundle.price) }}</span>
-			<div class="flex justify-around mt-3">
+			<div class="flex justify-center mt-3">
 				<v-button-filled id="read-more" @btnOnClickEvent="goToBundleDetails">
 					<svg-hero-icon>
 						<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
@@ -16,22 +16,6 @@
 								stroke-width="2"
 								d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253"
 							/>
-						</svg>
-					</svg-hero-icon>
-				</v-button-filled>
-				<v-button-filled id="add-to-cart">
-					<svg-hero-icon>
-						<svg
-							fill="none"
-							stroke-linecap="round"
-							stroke-linejoin="round"
-							stroke-width="2"
-							viewBox="0 0 24 24"
-							stroke="currentColor"
-						>
-							<path
-								d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
-							></path>
 						</svg>
 					</svg-hero-icon>
 				</v-button-filled>
--- src/resources/js/Components/Forms/VButtonIcon.vue
+++ src/resources/js/Components/Forms/VButtonIcon.vue
@@ -3,6 +3,7 @@
 		:disabled="isDisabled"
 		@click="(event) => $emit('btnOnClickEvent', event)"
 		class="text-gray-500 focus:outline-none focus:text-gray-600 hover:text-indigo-600"
+		:class="[isDisabled ? 'pointer-events-none' : '']"
 	>
 		<slot></slot>
 	</button>
